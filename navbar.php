997c4e9bc899cf2a32e24dc2411babdb78272a15
Nemanja Simic <nemanja@polantis.com>
2018-03-24 19:46:51 +0100

    trazi dodato
    trazi

--- navbar.php
+++ navbar.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top" style="height: 45px;">
-    
+
     <a class="navbar-brand" href="http://www.nsimic.com/">NS</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -7,7 +7,7 @@
 
     <div class="collapse navbar-collapse" id="navbarsExampleDefault">
         <ul class="navbar-nav mr-auto">
-           <li class="nav-item">
+            <li class="nav-item">
                 <a class="nav-link" href="/cv.php">CV</a>
             </li>
 
@@ -46,16 +46,16 @@
             <li class="nav-item">
                 <a class="nav-link" href="/gallery.php?nom=09_nantes">Nantes</a>
             </li>
-            
+
             <li class="nav-item">
                 <a class="nav-link" href="/gallery.php?nom=10_Saint Malo">Saint Malo</a>
             </li>
         </ul>
-        <!--
-         <form class="form-inline my-2 my-lg-0">
+
+        <form class="form-inline my-2 my-lg-0">
             <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Trazi</button>
         </form>
--->
+
     </div>
 </nav>
