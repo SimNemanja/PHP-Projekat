c32ebec43547e45a2cfe6b1f34d85198c0c4c40e
Nemanja Simic <nemanja@polantis.com>
2018-03-24 19:58:58 +0100

    jebi se

--- navbar.php
+++ navbar.php
@@ -52,12 +52,10 @@
             </li>
         </ul>
 
-<!--
         <form class="form-inline my-2 my-lg-0">
             <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Trazi</button>
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Jebi se</button>
         </form>
--->
 
     </div>
 </nav>
