cf33713795103ae6276aeca3c7d61a312061159f
Nemanja Simic <nemanja@polantis.com>
2018-03-24 21:34:34 +0100

    Liste za izbor fotgrafija
    Liste za izbor fotografija funkcionisu, carousel ne, glavna stranica je cv, svi meniji su dobri.

---
+++ index_nefunkcionalna-carousel.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+<!--    W3CSS-->
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+<!-- Bootstrap core CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+
+    <!-- Custom styles for this template -->
+<link rel="stylesheet" href="css/main.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-a  wesome/4.7.0/css/font-awesome.min.css">
+    
+<style>
+</style>
+    
+</head>
+<body>
+        
+<?php include("navbar.php"); ?>
+<?php include("carousel.php"); ?>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+</body>
+</html>
--- navbar.php
+++ navbar.php
@@ -1,26 +1,41 @@
-
 <div class="navbar-N">
-    
+
     <a href="http://www.nsimic.com/">Nemanja SIMIC</a>
-    <a href="/cv.php">CV</a>
-    <a href="/gallery.php?nom=Dessins">Dessins</a>
-    
+    <!--    <a href="/cv.php">CV</a>-->
+    <a href="/gallery.php?nom=D1_Dessins">Dessins</a>
+    <a href="#">Photos</a>
+
     <div class="dropdown-N">
-        <button class="dropbtn-N">Photos
+        <button class="dropbtn-N">France
       <i class="fa fa-caret-down"></i>
     </button>
         <div class="dropdown-N-content">
-            <a href="/gallery.php?nom=01_rome">Rome</a>
-            <a href="/gallery.php?nom=02_loire">Loire</a>
-            <a href="/gallery.php?nom=03_belgium">Belgium</a>
             <a href="/gallery.php?nom=04_normandy">Normandy</a>
             <a href="/gallery.php?nom=05_chantilly">Chantilly</a>
             <a href="/gallery.php?nom=06_strasbourg">Strasbourg</a>
-            <a href="/gallery.php?nom=07_amsterdam">Amsterdam</a>
-            <a href="/gallery.php?nom=08_naples">Naples</a>
             <a href="/gallery.php?nom=09_nantes">Nantes</a>
             <a href="/gallery.php?nom=10_Saint Malo">Saint Malo</a>
         </div>
     </div>
-    
+
+    <div class="dropdown-N">
+        <button class="dropbtn-N">Italy
+      <i class="fa fa-caret-down"></i>
+    </button>
+        <div class="dropdown-N-content">
+            <a href="/gallery.php?nom=01_rome">Rome</a>
+            <a href="/gallery.php?nom=08_naples">Naples</a>
+        </div>
+    </div>
+
+    <div class="dropdown-N">
+        <button class="dropbtn-N">Europe
+      <i class="fa fa-caret-down"></i>
+    </button>
+        <div class="dropdown-N-content">
+            <a href="/gallery.php?nom=03_belgium">Belgium</a>
+            <a href="/gallery.php?nom=07_amsterdam">Amsterdam</a>
+        </div>
+    </div>
+
 </div>
