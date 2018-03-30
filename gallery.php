32d571debe0c11a747173b2b8a4ca783022692cb
Nemanja Simic <nemanja@polantis.com>
2018-03-30 14:00:27 +0200

    nova jquery verzija

--- gallery.php
+++ gallery.php
@@ -18,7 +18,7 @@
     <link rel="stylesheet" href="css/NSgallery.css">
 
     <!--   Scripts -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/js/bootstrap.min.js"></script>
     <script src="js/lightbox.js"></script>
     <script src="js/jquery.justifiedGallery.js"></script>
